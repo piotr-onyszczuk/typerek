ca09a9a3787604f7222a574bf8c0a59dbe4993c9
piotr.onyszczuk <piotr.onyszczuk@bakk.com>
2021-10-03 22:12:25 +0200

    Initial

---
+++ Typerek/Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Typerek.Database
+{
+    public class ApplicationDbContext : EntityContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"");
+        }
+    }
+}
